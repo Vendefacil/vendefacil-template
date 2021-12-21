d8b8e0a129e7fa80a28a14788fb605df5154f370
jairo_rauda <jairo.rauda@neoris.com>
2021-12-20 23:45:18 -0600

    Updates

---
+++ precache-manifest.8ce0b7d2fbd751597292bcc1199982e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575e124c7550aa8136fc059d2c4a8cbb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a759805aa3d53f14cc0",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "4283a335b2de78c7ca1a",
+    "url": "./static/js/2.ccc58029.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ccc58029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a759805aa3d53f14cc0",
+    "url": "./static/js/main.4d8ae40a.chunk.js"
+  },
+  {
+    "revision": "bd1b3ac8f4658139aa24",
+    "url": "./static/js/runtime-main.aafdc22f.js"
+  }
+]);
---
+++ static/js/main.4d8ae40a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvendefacil-template-example"]=this["webpackJsonpvendefacil-template-example"]||[]).push([[0],{14:function(e,n,t){"use strict";t.r(n);t(9);var a=t(0),l=t.n(a),c=t(5),r=t.n(c),o=(t(3),t(6),t(7),function(){var e=function(e){return"<%= ".concat(e," %>")};return l.a.createElement(l.a.Fragment,null,l.a.createElement("h1",null,"VendeFacil Template"),e("store.name"))});r.a.render(l.a.createElement(o,null),document.getElementById("root"))},8:function(e,n,t){e.exports=t(14)},9:function(e,n,t){}},[[8,1,2]]]);
+//# sourceMappingURL=main.4d8ae40a.chunk.js.map
